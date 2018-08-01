4fbd5df19d2a472c8683b3c0b80bf4437113d81e
preeti singh <preeti.singh.3010@gmail.com>
2018-08-01 20:26:22 +0100

    first

---
+++ Expense.aspx
@@ -0,0 +1,172 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Expense.aspx.cs" Inherits="Expense" MasterPageFile="~/Home.master"  EnableEventValidation="false"%>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="heads" Runat="Server">
+
+<%@ Register Assembly="AjaxControlToolkit" Namespace="AjaxControlToolkit" TagPrefix="ajaxToolkit" %>
+
+
+    <!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+    <title></title>
+  
+    <link href="css/site.css" rel="stylesheet" type="text/css" />
+    <link href="css/theme.css" rel="stylesheet" type="text/css" />
+    <link href="css/triangle.css" rel="stylesheet" type="text/css" />
+    
+    <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
+    <style type="text/css">
+       
+        #img{
+            vertical-align: middle;
+        }
+     
+        .RowStyle {
+            height: 150px;
+        }
+        .txtbox
+        {
+            border-top-left-radius: 4px;
+            border-top-right-radius: 4px;
+            border-bottom-left-radius: 4px;
+            border-bottom-right-radius: 4px;
+        }
+    .auto-style1 {
+        height: 29px;
+    }
+    #form1{
+        font-family:Bahnschrift SemiBold;
+        font-size:18px;
+    }
+    </style>
+       
+
+
+</head>
+<body >
+    <form id="form1" runat="server" style="background-color:#E9E9E9">
+        
+        <div style="width:1000px;margin:0 auto;padding-top:20px;">
+
+            <table style="width:100%;margin:0 auto;">
+                <tr>
+                    <td >
+                       
+                       Date <%--<asp:Label ID="lbl_date" runat="server" Text="Date" Font-Names="Bahnschrift SemiBold" ></asp:Label>--%>
+                       
+                    </td>
+                    <td>
+                        <asp:TextBox ID="TextBox1" runat="server" AutoCompleteType="Disabled"    MaxLength="50" CssClass="txtbox"></asp:TextBox>
+                        <asp:Image ID="Img" runat="server" ImageUrl="~/Pics/Calendar.png"  Width="22px" CssClass="txtbox" />
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator1" runat="server" ErrorMessage="Please enter Date" ControlToValidate="Textbox1" ValidationGroup="Require" ForeColor="#FF3300"></asp:RequiredFieldValidator> 
+                    </td>
+                   <td>
+                       <ajaxToolkit:CalendarExtender ID="CalendarExtender1" runat="server"  TargetControlID="TextBox1" Format="dd/MM/yyyy"/></td>
+                                      
+                </tr>
+                <tr>
+                    <td >
+                        Amount
+                       <%-- <asp:Label ID="lbl_Money" runat="server" Text="Amount" Font-Names="Bahnschrift SemiBold" ></asp:Label>--%>
+                    </td>
+                    <td>
+                      <asp:TextBox ID="txt_Money" runat="server" AutoCompleteType="Disabled" MaxLength="50" CssClass="txtbox" ></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator2" runat="server" ErrorMessage="Please enter Amount" ControlToValidate="txt_Money" ValidationGroup="Require" ForeColor="#FF3300"></asp:RequiredFieldValidator> 
+                          <asp:RegularExpressionValidator id="RegularExpressionValidator1"
+                   ControlToValidate="txt_Money" ForeColor="#FF3300"
+                   ValidationExpression="\d+" ValidationGroup="Require"
+                   Display="Static"
+                   EnableClientScript="true"
+                   ErrorMessage="Please enter numbers only"
+                   runat="server"/>  
+                    </td>
+                </tr>
+                <tr>
+                    <td class="auto-style1" >
+                        Payment
+                        <%--<asp:Label ID="lbl_pym" runat="server" Text="Payment" Font-Names="Bahnschrift SemiBold" ></asp:Label>--%>
+                    </td>
+                    <td class="auto-style1" >
+                        
+                        <asp:DropDownList ID="dropdownlist" runat="server" CssClass="txtbox" Font-Names="Bahnschrift SemiBold">
+                            
+                        </asp:DropDownList>                  
+                         <asp:Button ID="btn_payment" runat="server" Text="Add Payment" Height="22px" Width="95px"  OnClick="btn_payment_Click" CssClass="txtbox" Font-Names="Bahnschrift SemiBold" />
+                        <asp:Button ID="btnHidden" runat="Server" style="display:none" OnClick="btnHidden_Click" />
+                       
+                    </td>
+                </tr>
+                <tr class="RowStyle">
+                    <td >
+                        Description
+                      <%--  <asp:Label ID="lbl_des" runat="server" Text="Description" Font-Names="Bahnschrift SemiBold"></asp:Label>--%>
+                    </td>
+                    <td >
+                        <asp:TextBox ID="txt_Description" runat="server" Height="100px" TextMode="MultiLine" AutoCompleteType="Disabled" CssClass="txtbox" ></asp:TextBox>
+                        <asp:RequiredFieldValidator ID="RequiredFieldValidator3" runat="server" ErrorMessage="Please enter Description" ControlToValidate="txt_Description" ValidationGroup="Require" ForeColor="#FF3300"></asp:RequiredFieldValidator> 
+                    </td>
+                </tr>
+                <tr>
+                    <td >
+                        Comments
+                       <%-- <asp:Label ID="lbl_Comment" runat="server" Text="Comments" Font-Names="Bahnschrift SemiBold"></asp:Label>--%>
+                    </td>
+                    <td >
+                        <asp:TextBox ID="txt_Comment" runat="server" Height="100px" TextMode="MultiLine" AutoCompleteType="Disabled" CssClass="txtbox"></asp:TextBox>
+                    </td>
+                </tr>
+                <tr>
+                    <td>Receipt(Jpg/Png)
+                        <%--<asp:Label ID="lbl_Image" runat="server" Text="Receipt(Jpg/Png)" Font-Names="Bahnschrift SemiBold"  ></asp:Label>--%>
+                    </td>
+                    <td>  
+                        <asp:ScriptManager ID="ScriptManager1" runat="server"></asp:ScriptManager>
+                        <asp:UpdatePanel ID="UpdatePanel1" runat="server" UpdateMode="Conditional">
+                            <ContentTemplate>
+                                <asp:FileUpload ID="FileUpload1" runat="server" CssClass="txtbox" Font-Names="Bahnschrift SemiBold"/>
+                                <asp:Button ID="btn_upload" runat="server" Text="Show Image" OnClick="btn_upload_Click" CssClass="txtbox" Font-Names="Bahnschrift SemiBold" />
+                                <asp:Image ID="Image1" runat="server" Height="150px" Width="200px" ImageAlign="Middle" />
+                            </ContentTemplate>
+                            <Triggers>
+                                <asp:PostBackTrigger ControlID="btn_upload" />
+                            </Triggers>
+                        </asp:UpdatePanel>
+                    </td>
+                </tr>
+            </table>
+           
+
+        </div>
+
+        <div style="margin:0 auto;width:300px;">
+             <table >
+                <tr>
+
+                    <td></td>
+
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">&nbsp;</td>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td class="auto-style4">
+                        <asp:Button ID="btn_submit" runat="server" OnClick="btn_submit_Click" Text="Submit" CssClass="txtbox" Font-Names="Bahnschrift SemiBold" ValidationGroup="Require"/>
+                    </td>
+                    <td>
+                        <asp:Button ID="btn_reports" runat="server" OnClick="btn_reports_Click" Text="Reports" CssClass="txtbox" Font-Names="Bahnschrift SemiBold"/>
+                    </td>
+                     
+                </tr>
+            </table>
+        </div>
+    </form>
+</body>
+</html>
+</asp:Content>
---
+++ Report.aspx.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.text.html.simpleparser;
+using System.Text.RegularExpressions;
+using System.Drawing;
+
+public partial class Report : System.Web.UI.Page 
+{
+
+    public SqlConnection con;
+    public SqlDataAdapter adp;
+    public DataSet ds;
+    private static DataTable dt = new DataTable();
+
+    public object ImageDataFactory { get; private set; }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        con = (SqlConnection)Session["connection"];
+        Page.ClientScript.RegisterStartupScript(typeof(Page), System.DateTime.Now.Ticks.ToString(), "scrollTo('" + autoScroll.ClientID + "');", true);
+        if (data_grid.Rows.Count == 0)
+        {
+            btn_Excel.Visible = false;
+        }
+        else
+        {
+            btn_Excel.Visible = true;
+        }
+    }
+    public void loaddata()
+    {
+
+        data_grid.DataSource = (DataTable)Session["data_grid"];
+        data_grid.DataBind();
+        if (data_grid.Rows.Count == 0)
+        {
+            btn_Excel.Visible = false;
+        }
+        else
+        {
+            btn_Excel.Visible = true;
+        }
+        con.Close();
+    }
+
+    protected void drp_month_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        string query;
+        var result = drp_month.Text;
+        query = "select * from tbl_expenses where MONTH(Date)='" + result + "'";
+        con.Open();
+        SqlCommand sqlcmd = new SqlCommand(query, con);
+        adp = new SqlDataAdapter(sqlcmd);
+        ds = new DataSet();
+        adp.Fill(ds);
+        dt = ds.Tables[0];
+        Session["data_grid"] = dt;
+        loaddata();
+        con.Close();
+    }
+
+    protected void data_grid_RowDeleting(object sender, GridViewDeleteEventArgs e)
+    {
+        con.Open();
+        DataTable dt = (DataTable)Session["data_grid"];
+        dt.Rows.RemoveAt(e.RowIndex);
+        Session["data_grid"] = dt;
+        GridViewRow row = (GridViewRow)data_grid.Rows[e.RowIndex];
+        SqlCommand cmd = new SqlCommand("delete FROM tbl_expenses where id='" + Convert.ToInt32(data_grid.DataKeys[e.RowIndex].Values[0].ToString()) + "'", con);
+        cmd.ExecuteNonQuery();
+        loaddata();
+        con.Close();
+
+    }
+
+    protected void data_grid_RowEditing(object sender, GridViewEditEventArgs e)
+    {
+        data_grid.EditIndex = e.NewEditIndex;
+        loaddata();
+    }
+    string filename;
+    protected void data_grid_RowUpdating(object sender, GridViewUpdateEventArgs e)
+    {
+        int id = Convert.ToInt32(data_grid.DataKeys[e.RowIndex].Value.ToString());
+        GridViewRow row = (GridViewRow)data_grid.Rows[e.RowIndex];
+        DataTable dt = (DataTable)Session["data_grid"];
+        TextBox textDate = (TextBox)row.Cells[2].Controls[0];
+        TextBox textMoney = (TextBox)row.Cells[3].Controls[0];
+        TextBox textPayment = (TextBox)row.Cells[4].Controls[0];
+        TextBox textDescription = (TextBox)row.Cells[5].Controls[0];
+        TextBox textComment = (TextBox)row.Cells[6].Controls[0];
+        FileUpload FileUpload1 = (FileUpload)data_grid.Rows[e.RowIndex].FindControl("FileUpload1");
+        string path = "/Upload/";
+        if (FileUpload1.HasFile)
+        {
+            path += FileUpload1.FileName.Replace(" ","");
+            filename = FileUpload1.FileName.Replace(" ", "");
+            //save image in folder    
+            FileUpload1.SaveAs(MapPath(path));
+        }
+        else
+        {
+            // use previous user image if new image is not changed 
+
+            System.Web.UI.WebControls.Image img = (System.Web.UI.WebControls.Image)data_grid.Rows[e.RowIndex].FindControl("img_user");
+            if (img.ImageUrl != "")
+            {
+                path = img.ImageUrl;
+                int pos = img.ImageUrl.LastIndexOf("/") + 1;
+                filename = path.Substring(pos, path.Length - pos);
+            }
+            else
+            {
+                path = "/Upload/download.jpg";
+                filename = "download.jpg";
+            }
+        }
+        var date = Convert.ToDateTime(textDate.Text).ToString("yyyy-MM-dd");
+
+        con.Open();
+
+        SqlCommand cmd = new SqlCommand("update tbl_expenses set Money='" + textMoney.Text + "',Payment='" + textPayment.Text + "',Description='" + textDescription.Text + "',Comments='" + textComment.Text + "',Image='" + path + "',Filename='" + filename + "',Date='" + date + "' where id='" + id + "'", con);
+        //SqlCommand cmd = new SqlCommand("update tbl_expenses set Money='" + textMoney.Text + "',Payment='" + textPayment.Text + "',Description='" + textDescription.Text + "',Image='" + path + "',Date='" + date + "' where id='" + id + "'", con);
+        cmd.ExecuteNonQuery();
+        dt.Rows[row.RowIndex]["Money"] = textMoney.Text;
+        dt.Rows[row.RowIndex]["Date"] = date;
+        dt.Rows[row.RowIndex]["Payment"] = textPayment.Text;
+        dt.Rows[row.RowIndex]["Description"] = textDescription.Text;
+        dt.Rows[row.RowIndex]["Comments"] = textComment.Text;
+        dt.Rows[row.RowIndex]["Image"] = path;
+        Session["data_grid"] = dt;
+        data_grid.EditIndex = -1;
+        con.Close();
+        loaddata();
+    }
+
+    protected void data_grid_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
+    {
+        data_grid.EditIndex = -1;
+        loaddata();
+    }
+
+    protected void btn_find_Click(object sender, EventArgs e)
+    {
+        //DataSet ds = new DataSet();
+        string query;
+        var Startdate = Convert.ToDateTime(txt_StartDate.Text).ToString("yyyy-MM-dd");
+        var Enddate = Convert.ToDateTime(txt_Enddate.Text).ToString("yyyy-MM-dd");
+        query = "select * from tbl_expenses where date>= '" + Startdate + "' and " + "date<= '" + Enddate + "'";
+        con.Open();
+        SqlCommand sqlcmd = new SqlCommand(query, con);
+        adp = new SqlDataAdapter(sqlcmd);
+        ds = new DataSet();
+        adp.Fill(ds);
+        dt = ds.Tables[0];
+        if (dt.Rows.Count == 0)
+        {
+            btn_Excel.Visible = false;
+        }
+        else
+        {
+            btn_Excel.Visible = true;
+        }
+        Session["data_grid"] = dt;
+        loaddata();
+        con.Close();
+    }
+
+    protected void drp_yearly_SelectedIndexChanged(object sender, EventArgs e)
+    {
+
+        string query;
+        var result = drp_yearly.SelectedItem.Text;
+        if (result == "--Select--")
+        {
+            result = "0";
+        }
+        query = "select * from tbl_expenses where Year(Date)='" + result + "'";
+        con.Open();
+        SqlCommand sqlcmd = new SqlCommand(query, con);
+        adp = new SqlDataAdapter(sqlcmd);
+        ds = new DataSet();
+        adp.Fill(ds);
+        dt = ds.Tables[0];
+        Session["data_grid"] = dt;
+        loaddata();
+        con.Close();
+
+    }
+
+    protected void btn_Excel_Click(object sender, EventArgs e)
+    {
+        if (data_grid.Rows.Count > 0)
+        {
+            Response.Clear();
+            Response.Buffer = true;
+            Response.ClearContent();
+            Response.ClearHeaders();
+            Response.Charset = "";
+            string FileName = "Expense" + DateTime.Now + ".xls";
+            StringWriter strwritter = new StringWriter();
+            HtmlTextWriter htmltextwrtter = new HtmlTextWriter(strwritter);
+            Response.Cache.SetCacheability(HttpCacheability.NoCache);
+            Response.ContentType = "application/vnd.ms-excel";
+            Response.AddHeader("Content-Disposition", "attachment;filename=" + FileName);
+            data_grid.Columns[1].Visible = false;
+            data_grid.Columns[0].Visible = false;
+            data_grid.Columns[7].Visible = false;
+            data_grid.GridLines = GridLines.Both;
+            data_grid.HeaderStyle.Font.Bold = true;
+            data_grid.RenderControl(htmltextwrtter);
+            Response.Write(strwritter.ToString());
+            Response.End();
+        }
+    }
+
+    public override void VerifyRenderingInServerForm(Control control)
+    {
+
+    }
+    //protected string GetUrl(string imagepath)
+
+    //{
+
+    //    string[] splits = Request.Url.AbsoluteUri.Split('/');
+
+    //    if (splits.Length >= 2)
+
+    //    {
+
+    //        string url = splits[0] + "//";
+
+    //        for (int i = 2; i < splits.Length - 1; i++)
+
+    //        {
+
+    //            url += splits[i];
+
+    //            url += "/";
+
+    //        }
+
+    //        return url + imagepath;
+
+    //    }
+
+    //    return imagepath;
+
+    //}
+    protected void btn_Pdf_Click(object sender, EventArgs e)
+    {
+        iTextSharp.text.Font font2 = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8f, iTextSharp.text.Font.BOLD | iTextSharp.text.Font.UNDERLINE, iTextSharp.text.BaseColor.RED);
+        Response.ContentType = "application/pdf";
+        string FileName = "Expense" + DateTime.Now + ".pdf";
+        Response.AddHeader("content-disposition", "attachment;filename=" + FileName);
+        Response.Cache.SetCacheability(HttpCacheability.NoCache);
+        data_grid.Columns[1].Visible = false;
+        data_grid.Columns[0].Visible = false;
+        data_grid.Columns[2].Visible = false;
+        StringWriter sw = new StringWriter();
+        HtmlTextWriter hw = new HtmlTextWriter(sw);
+        data_grid.RenderControl(hw);
+        DataTable dt = new DataTable();
+        dt = (DataTable)Session["data_grid"];
+        Boolean columnExists = dt.Columns.Contains("ID");
+        Boolean columnPageno = dt.Columns.Contains("PageNo");
+        if (columnExists)
+        {
+            dt.Columns.Remove("ID");
+            dt.Columns.Remove("Image");
+        }
+        if (!columnPageno) { 
+        dt.Columns.Add("PageNo");
+        }
+        List<string> ImageStr = GetImagesInHTMLString(sw.ToString());        
+        var unique_items = new HashSet<string>(ImageStr);
+        IDictionary<int, string> dict = new Dictionary<int, string>();         
+        Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 0f);
+        //HTMLWorker htmlparser = new HTMLWorker(pdfDoc);
+        var writer=PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+        pdfDoc.Open();
+
+        //Create object for image
+        int count = 1;   
+        foreach (string s in unique_items)
+        {
+            int pos = s.LastIndexOf("\\") + 1;
+            filename = s.Substring(pos, s.Length - pos);
+            pdfDoc.NewPage();
+            dict.Add(count, filename);
+            iTextSharp.text.Image gif = iTextSharp.text.Image.GetInstance(s);
+            gif.Alignment = iTextSharp.text.Image.TEXTWRAP | iTextSharp.text.Image.ALIGN_RIGHT;
+            gif.ScalePercent(24f);
+            gif.IndentationLeft = 9f;
+            gif.SetAbsolutePosition((PageSize.A4.Width - gif.ScaledWidth) / 2, (PageSize.A4.Height - gif.ScaledHeight) / 2);
+            gif.SpacingAfter = 9f;
+            pdfDoc.Add(gif);
+            count++;
+
+            Chunk chunk = new Chunk("Go to Last Page", font2);
+            //var des = new PdfDestination(PdfDestination.XYZ, 0, pdfDoc.PageSize.Height, 1.40f);
+            //PdfAction action = PdfAction.GotoLocalPage(2, des, a);
+            chunk.SetAction(new PdfAction(PdfAction.LASTPAGE));
+            Paragraph p5 = new Paragraph();
+            p5.Add(chunk);
+            pdfDoc.Add(p5);
+        }
+
+
+        for (int rows = 0; rows < dt.Rows.Count; rows++)
+        {
+                for (int count1 = 0; count1 < dict.Count; count1++)
+                {
+                    var element = dict.ElementAt(count1);
+                    var Key = element.Key;
+                    var Value = element.Value;
+
+                    if (dt.Rows[rows]["Filename"].ToString() == Value)
+                    {
+                        dt.Rows[rows]["PageNo"] = Key;
+                    }
+            }
+        }
+
+        
+        PdfPTable PdfTable1 = new PdfPTable(dt.Columns.Count);
+        PdfTable1.WidthPercentage = 100f;
+        
+        //PdfTable1.LockedWidth = true;
+        if (dt != null)
+        {
+            
+            PdfPCell PdfPCell = null;
+            //PdfPCell.BackgroundColor = iTextSharp.text.BaseColor.BLUE;
+            //Add Header of the pdf table
+            //iTextSharp.text.Font font = new iTextSharp.text.Font(iTextSharp.text.Font.NORMAL , 12);
+            iTextSharp.text.Font font = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 12f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLUE);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Date", font)));
+            PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Money", font)));
+            PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Payment", font)));
+            PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Description", font)));
+            PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Comments", font)));
+            PdfTable1.AddCell(PdfPCell);
+            //PdfPCell = new PdfPCell(new Phrase(new Chunk("Image", font)));
+            //PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("Link", font)));
+            PdfTable1.AddCell(PdfPCell);
+            PdfPCell = new PdfPCell(new Phrase(new Chunk("PageNo", font)));
+            PdfTable1.AddCell(PdfPCell);
+            //How add the data from datatable to pdf table
+            iTextSharp.text.Font font1 = new iTextSharp.text.Font(iTextSharp.text.Font.FontFamily.HELVETICA, 8f, iTextSharp.text.Font.BOLD, iTextSharp.text.BaseColor.BLACK);
+            
+            for (int rows = 0; rows < dt.Rows.Count; rows++)
+            {
+                for (int column = 0; column < dt.Columns.Count; column++)
+                {
+                    if (column == 5)
+                    {
+                        Chunk chunk = new Chunk(dt.Rows[rows]["Filename"].ToString(),font2);
+                        //Chunk chunk = new Chunk("Go to Page");
+                        var des = new PdfDestination(PdfDestination.XYZ, 0, pdfDoc.PageSize.Height, 0.99f);
+                        int pageno = int.Parse(dt.Rows[rows]["PageNo"].ToString());
+                        PdfAction action = PdfAction.GotoLocalPage(pageno, des, writer);
+                        chunk.SetAction(action);
+                        PdfPCell = new PdfPCell(new Paragraph(chunk));
+                        PdfTable1.AddCell(PdfPCell);
+                    }
+                    else
+                    {
+                        PdfPCell = new PdfPCell(new Paragraph(new Chunk(dt.Rows[rows][column].ToString(),font1)));
+                        PdfTable1.AddCell(PdfPCell);
+                    }
+                }
+            }
+            PdfTable1.SpacingBefore = 15f; // Give some space after the text or it may overlap the table
+           
+        }
+
+        //Chunk chunk = new Chunk("Go to page 3");
+        //PdfAction action = PdfAction.GotoLocalPage(3, new PdfDestination(0), a);
+        //chunk.SetAction(action);
+
+        //document.Add(chunk);
+
+        //Chunk chunk = new Chunk("Go to page 3");
+        //var des = new PdfDestination(PdfDestination.XYZ, 0, pdfDoc.PageSize.Height, 0.95f);
+        //PdfAction action = PdfAction.GotoLocalPage(2, des, writer);
+        //chunk.SetAction(action);
+        //Paragraph p5 = new Paragraph();
+        //p5.Add(chunk);
+        ////pdfDoc.Add(p5);
+        //string html2 = Regex.Replace(sw.ToString(), @"(<img\/?[^>]+>)", "", RegexOptions.IgnoreCase);
+        //StringReader sr = new StringReader(html2);
+        pdfDoc.NewPage();
+        pdfDoc.Add(PdfTable1);
+        pdfDoc.Close();
+        Response.Write(pdfDoc);
+        Response.End();
+        data_grid.AllowPaging = true;
+        data_grid.DataBind();
+    }
+
+    private List<string> GetImagesInHTMLString(string htmlString)
+    {
+        List<string> images = new List<string>();
+        string pattern = "<img.+?src=[\"'](.+?)[\"'].*?>";
+        Regex rgx = new Regex(pattern, RegexOptions.IgnoreCase);
+        MatchCollection matches = rgx.Matches(htmlString);
+        for (int i = 0, l = matches.Count; i < l; i++)
+        {
+            string modifyStr = matches[i].Groups[1].Value.Remove(0, 7);
+            images.Add(Server.MapPath("Upload") + modifyStr.Replace("/","\\").Replace("%20", " "));
+        }   
+        return images;
+    }
+
+    public void TableLayout(PdfPTable table, float[][] widths, float[] heights, int headerRows, int rowStart, PdfContentByte[] canvases)
+    {
+        throw new NotImplementedException();
+    }
+}
+
